925dc7cb268a160c0824961955cc80fc0e31978e
kumudini nimhan <kumudininimhan@gmail.com>
2017-05-14 12:51:23 +0530

    added registration controller

---
+++ src/main/resources/public/js/controllers/registrationController.js
@@ -0,0 +1,38 @@
+angular.module('meetupApp')
+.controller('registrationController', ['$scope', '$window', 'RegistrationService', function($scope, $window, RegistrationService) {    
+	
+	$scope.register = function (isValid) {
+		$scope.success = false;
+		$scope.error = false;
+
+		
+        if(isValid) {
+        	var addressInDTOs = [{
+        		street: $scope.street,
+        		city: $scope.city,
+        		country: $scope.country
+        	}];
+        	
+	        var userInDTO = {
+	        	name: $scope.name,
+	        	email: $scope.email,
+	        	password: $scope.password,
+	        	phone: $scope.phone,
+	        	addressInDTOs: addressInDTOs,
+	        };
+	        
+	        RegistrationService.save(userInDTO)
+	        .$promise.then(function (data) {
+            	$scope.success = true;
+            })
+            .catch(function(data) {
+            	$scope.error = true;
+            	$scope.errorMessage = data.data.message;
+            });
+        }
+    };
+    
+    $scope.cancel = function () {
+    	$window.location.reload();
+    };
+}]);
